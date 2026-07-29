493ec3c77265846044c552dfb98b6356e911cc05
Zenitsu <toonnettamil@gmail.com>
2026-07-29 19:20:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -24,11 +24,11 @@ DB_NAME = "yato"
 
 FSUBS = [[-1002172875461, True, 10]] # Force Subscription Channels [channel_id, request_enabled, timer_in_minutes]
 # Database Channel (Primary)
-DB_CHANNEL =    # just put channel id dont add ""
+DB_CHANNEL = -1002953776171   # just put channel id dont add ""
 # Multiple Database Channels (can be set via bot settings)
 # DB_CHANNELS = {
-#     "1002953776171": {"name": "Primary DB", "is_primary": True, "is_active": True},
-#     "1002953776171": {"name": "Secondary DB", "is_primary": False, "is_active": True}
+#     "-1002953776171": {"name": "Primary DB", "is_primary": True, "is_active": True},
+#     "-1002953776171": {"name": "Secondary DB", "is_primary": False, "is_active": True}
 # }
 # Auto Delete Timer (seconds)
 AUTO_DEL = 10800
